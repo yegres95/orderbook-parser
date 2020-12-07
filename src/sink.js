46d701103d86c865da249b6ea9add208a8ce6821
Sergey Sudakov <sergey95@gmail.com>
2020-12-07 04:24:16 +0000

    send updates per minute to client

--- src/sink.js
+++ src/sink.js
@@ -5,33 +5,41 @@ const { LOAD_ORDERBOOK } = require('./services/constants')
 const FREQUENCY = 10000;
 const UPDATE_FREQUENCY = 1000; // How often the user gets sent the updated orderbooks
 
-const pairsSending = {};
-
 class Sink {
 
   constructor(store) {
     this.store = store;
-    this.updates = [];
+    this.updates = {};
+    this.pairsSending = {};
+    this.updatesPerMinute = {};
 
     setInterval(() => {
-      const updatesPerMinute =
-        (this.updates.filter(u => u > Date.now() - FREQUENCY).length * 60000) /
-        FREQUENCY;
-  
-      console.log(`[sink] Speed: ${updatesPerMinute} orderbooks per minute`);
+      let pairs = Object.keys(this.pairsSending);
+      for (let i = 0; i < pairs.length; i++) {
+        let pair = pairs[i];
+        const updatesPerMinute =
+          (this.updates[pair].filter(u => u > Date.now() - FREQUENCY).length * 60000) /
+          FREQUENCY;
+    
+        updatesPerMinute[pair] = updatesPerMinute;
+        console.log(`[sink] Speed: ${updatesPerMinute} orderbooks per minute -> ${pair}`);
+      }
     }, FREQUENCY);
 
     setInterval(() => {
-      Object.keys(pairsSending).forEach(pair => {
-        wsSend({pair, data: this.store.getOrderbook(pair)}, LOAD_ORDERBOOK);
+      Object.keys(this.pairsSending).forEach(pair => {
+        wsSend({pair, updatesPerMinute: updatesPerMinute[pair], data: this.store.getOrderbook(pair)}, LOAD_ORDERBOOK);
       })
     }, UPDATE_FREQUENCY);
   }
 
   send(pair, params) {
-    this.updates.push(Date.now());
+    if (this.updates[pair] === undefined) {
+      this.updates[pair] = []
+    } 
+    this.updates[pair].push(Date.now());
     this.store.ingest(pair, params);
-    pairsSending[pair] = true
+    this.pairsSending[pair] = true
   }
 
 }
