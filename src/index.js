97baa7f9297ec5eedd23b85b17d8bd394da4ca2e
Sergey Sudakov <sergey95@gmail.com>
2020-12-07 00:30:54 +0000

    take out the market array into a seperate line

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 var http = require('http');
 
+const orderBooks = ["ETH/XBT", "XBT/USD"];
+
 const store = require("./store/index");
 const Kraken = require("./services/krakenMarket");
 const Client = require("./services/clientConnection").Client;
@@ -7,5 +9,5 @@ const Sink = require("./sink");
 
 const orderbookStore = new store.orderbook();
 const sink = new Sink(orderbookStore)
-const exchange = Kraken(["ETH/XBT", "XBT/USD"], sink);
+const exchange = Kraken(orderBooks, sink);
 const connections = Client(orderbookStore);
